ba31f2f6d9d42a142abe66c5dc64c6b02a99cd02
Roman <86870297+roman22rus@users.noreply.github.com>
2022-09-22 10:44:17 +0500

    Update first_tests.py

--- tests/first_tests.py
+++ tests/first_tests.py
@@ -3,11 +3,19 @@ from app.calculator import Calculator
 
 
 class TestCalc:
-   def setup(self):
+     def setup(self):
        self.calc =  Calculator
 
    def test_multiply_calculate_correctly(self):
        assert self.calc.multiply(self, 2, 2) == 4
 
-   def test_multiply_failed(self):
-        assert self.calc.multiply(self, 2, 2) == 5
+   def test_division_correctly(self):
+        assert self.calc.division(self, 6, 2) == 3
+
+
+   def test_subtraction_correctly(self):
+       assert self.calc.subtraction(self,10,6) == 4
+
+
+   def test_adding_correctly(self):
+       assert self.calc.adding(self,5,5) ==10
